93a22deb5152a89a3d31e0ae1f2be20178c4a0f9
Lehman <Jason.Lehman@Spectrumhealth.org>
2020-09-05 14:46:05 -0400

    Updating script

--- CreateServiceBus.ps1
+++ CreateServiceBus.ps1
@@ -1,9 +1,15 @@
 param(
-  [Parameter(Mandatory = $true)] [string]$ResourceGroup,
-  [Parameter(Mandatory = $true)] [string]$NameSpace,
+  [Parameter(Mandatory = $true)] [string]$Subscription, # Name of Existing Subsription
+  [Parameter(Mandatory = $true)] [string]$ResourceGroup, # Name of Existing Resource Group
+  [Parameter(Mandatory = $true)] [string]$NameSpace, # Desired NameSpace
+  [Parameter(Mandatory = $true)] [string]$Location, # Location of SeviceBus.  i.e eastus
   [Parameter(Mandatory = $true)] [string]$Queues # Multiple queues separated by comma
 )
-New-AzServiceBusNamespace -ResourceGroupName $ResourceGroup -Name $NameSpace -Location eastus
+
+$Cred = get-Credential
+Connect-AzAccount -Credential $Cred -Subscription $Subscription
+
+New-AzServiceBusNamespace -ResourceGroupName $ResourceGroup -Name $NameSpace -Location $Location
 $QueueArray = $Queues.Split(",")
 Foreach ($Queue in $QueueArray) {
 	New-AzServiceBusQueue -ResourceGroupName $ResourceGroup -NamespaceName $NameSpace -Name $Queue
